77e74fca9170c855fd490d10918b4960d7e3621d
chessai <chessai1996@gmail.com>
2024-10-18 05:11:02 -0500

    write assets

--- skylanders_nfc_data.h
+++ skylanders_nfc_data.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <furi.h>
+#include <storage/storage.h>
 #include <gui/gui.h>
 #include <gui/icon_i.h>
 #include <gui/view_dispatcher.h>
@@ -13,6 +14,7 @@
 #include <gui/modules/text_input.h>
 #include <gui/modules/byte_input.h>
 #include <gui/modules/widget.h>
+#include "skylanders_data/headers/skylanders_assets.h"
 
 #define NFC_APP_FOLDER    ANY_PATH("nfc")
 #define NFC_APP_EXTENSION ".nfc"
@@ -24,6 +26,8 @@
 
 // Failure modes
 // 1. target directory already exists
+
+/*
 typedef enum {
     Spyros_Adventure,
     Giants,
@@ -33,28 +37,72 @@ typedef enum {
     Superchargers,
     Imaginators,
 } SkylandersGame;
+*/
 
 // Returns the name of the game as a formatted string.
 // If somehow this switch statement is not exhaustive, it will log an error and return NULL.
-char* skylanders_game_to_string(SkylandersGame game) {
-    switch(game) {
-    case Spyros_Adventure:
+char* skylanders_game_to_string(const char* game) {
+    if(strcmp(game, "Spyros_Adventure") == 0) {
         return "Skylanders 1 - Spyro's Adventure";
-    case Giants:
+    } else if(strcmp(game, "Giants") == 0) {
         return "Skylanders 2 - Giants";
-    case Swap_Force:
+    } else if(strcmp(game, "Swap_Force") == 0) {
         return "Skylanders 3 - Swap Force";
-    case Trap_Team:
+    } else if(strcmp(game, "Trap_Team") == 0) {
         return "Skylanders 4 - Trap Team";
-    case Eons_Elite:
+    } else if(strcmp(game, "Eons_Elite") == 0) {
         return "Skylanders 4.5 - Eon's Elite";
-    case Superchargers:
+    } else if(strcmp(game, "Superchargers") == 0) {
         return "Skylanders 5 - Superchargers";
-    case Imaginators:
+    } else if(strcmp(game, "Imaginators") == 0) {
         return "Skylanders 6 - Imaginators";
+    } else {
+        FURI_LOG_E(APP_TAG, "Unknown SkylandersGame: %s", game);
+        return NULL;
     }
-    FURI_LOG_E(APP_TAG, "Unknown SkylandersGame: %d", game);
-    return NULL;
+}
+
+bool write_asset(Asset* asset) {
+    char path_buffer[512];
+    snprintf(
+        path_buffer,
+        512, // 512 should fit anything possible
+        "%s%s%s%s%s%s%s%s%s",
+        NFC_APP_FOLDER,
+        "/",
+        "skylanders",
+        "/",
+        skylanders_game_to_string(asset->game),
+        "/",
+        asset->type,
+        "/",
+        asset->name);
+
+    Storage* storage = furi_record_open(RECORD_STORAGE);
+    File* file = storage_file_alloc(storage);
+
+    bool result = storage_file_open(file, path_buffer, FSAM_WRITE, FSOM_CREATE_ALWAYS);
+    if(result) {
+        storage_file_write(file, asset->data, asset->data_len);
+        storage_file_close(file);
+    } else {
+        FURI_LOG_E("Failed to open or create file: %s", path_buffer);
+    }
+
+    storage_file_free(file);
+    furi_record_close(RECORD_STORAGE);
+    return result;
+}
+
+bool write_skylanders_nfc_data() {
+    bool result = true;
+    initialize_assets();
+
+    for(int i = 0; i < ASSET_COUNT; i++) {
+        result &= write_asset(assets[i]);
+    }
+
+    return result;
 }
 
 typedef enum {
